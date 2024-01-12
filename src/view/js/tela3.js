0fe86c680351b347cbe2a1a499c9bd52fcb40a9a
Felipe Gabriel Kretzer <felipegk2006@gmail.com>
2024-01-12 14:42:55 -0300

    :sparkles: feat: Botão clicável new alarm

---
+++ src/view/js/tela3.js
@@ -0,0 +1,5 @@
+var chamarTela4 = document.getElementById('idBuscarTelaNovoAlarme')
+chamarTela4.addEventListener("click", tratamentoChamarTela4)
+function tratamentoChamarTela4() {
+    window.location.href = "/newAlarmPage"
+}
