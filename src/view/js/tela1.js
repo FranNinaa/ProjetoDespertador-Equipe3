35d0168054fbfa68d58515c22f8512480b5b23d7
FranNinaa <francinesantoss85@gmail.com>
2024-01-12 16:48:26 -0300

    :sparkles:feat:9.004:Feito a implemetação das cores conforme a hora do dia

--- src/view/js/tela1.js
+++ src/view/js/tela1.js
@@ -1,6 +1,34 @@
+// Função para atualizar a cor de fundo com base na hora atual
+function updateBackgroundColor() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+
+    const timeInMinutes = hours * 60 + minutes;
+
+    let backgroundColor; 
+
+    if (timeInMinutes >= 300 && timeInMinutes < 720) {
+        const sunriseColors = ['#ffe0bf', '#f3b3a9', '#e88ea0', '#c885b9', '#9863b1'];
+        backgroundColor = sunriseColors[Math.floor((timeInMinutes - 300) / 84)];
+    }
+    else if (timeInMinutes >= 720 && timeInMinutes <= 1470) {
+        const afternoonColors = ['#d1f5f3', '#aeecf3', '#5fd7f9', '#54b3ed', '#fefed9'];
+        backgroundColor = afternoonColors[Math.floor((timeInMinutes - 720) / 150)];
+    }
+    else {
+        const nightColors = ['#dbdafc', '#9594c8', '#4c4690', '#3d3473', '#373252'];
+        let nightTime = timeInMinutes > 1320 ? timeInMinutes - 1320 : timeInMinutes + 240;
+        backgroundColor = nightColors[Math.floor(nightTime / 72)];
+    }
+    document.querySelector('.background').style.backgroundColor = backgroundColor;
+}
+
+
 document.querySelector("#adiarAlarme").style.display = "none"
 let diaAtual = new Date().getDate();
 
+//função de buscar a hora atual
 function fetchCurrentTime() {
     fetch('/horaCerta')
         .then(response => response.json())
@@ -13,7 +41,7 @@ function fetchCurrentTime() {
         .catch(error => console.error('Erro ao buscar a hora atual:', error));
 
 }
-
+//função de buscar o climas
 function getClima() {
     fetch('/clima?cidade=Blumenau')
         .then(response => response.json())
@@ -31,18 +59,19 @@ function getClima() {
 
 let ultimaHoraVerificada = null;
 
+// Função para atualizar a data
 function fetchDate() {
     const hoje = new Date();
     const diaHoje = hoje.getDate();
 
     fetch('/hoje')
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('data-atual').textContent = data.dataCerta;
-                diaAtual = diaHoje; // Atualiza o dia atual para o novo dia
-                console.log(data)
-            })
-            .catch(error => console.error('Erro ao buscar a data atual:', error));
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('data-atual').textContent = data.dataCerta;
+            diaAtual = diaHoje; 
+            console.log(data)
+        })
+        .catch(error => console.error('Erro ao buscar a data atual:', error));
 
     // Se mudou para um novo dia, busca a data formatada e atualiza a tela
     if (diaHoje !== diaAtual) {
@@ -50,21 +79,21 @@ function fetchDate() {
             .then(response => response.json())
             .then(data => {
                 document.getElementById('data-atual').textContent = data.dataCerta;
-                diaAtual = diaHoje; // Atualiza o dia atual para o novo dia
+                diaAtual = diaHoje;
                 console.log(data)
             })
             .catch(error => console.error('Erro ao buscar a data atual:', error));
     }
 }
-
+//função para buscar a mensagem
 function fetchMensagem() {
-        fetch('/msn')
-            .then(response => response.json())
-            .then(data => {
-                const mensagemElement = document.getElementById('mensagem');
-                mensagemElement.textContent = data.mensagem.msn;
-            })
-            .catch(error => console.error('Erro ao buscar a mensagem:', error));
+    fetch('/msn')
+        .then(response => response.json())
+        .then(data => {
+            const mensagemElement = document.getElementById('mensagem');
+            mensagemElement.textContent = data.mensagem.msn;
+        })
+        .catch(error => console.error('Erro ao buscar a mensagem:', error));
 }
 
 // Função que será chamada quando a página carregar
@@ -73,12 +102,14 @@ function onLoad() {
     getClima(); // Chama a função de buscar o clima
     fetchDate(); // Chama a função para atualizar a data inicialmente
     fetchMensagem(); // Chama a função para buscar a mensagem inicialmente
-  
-
+    updateBackgroundColor(); // Chama a função para atualizar a cor de fundo inicialmente'
 }
 
 // Atualiza a hora a cada segundo
 setInterval(fetchCurrentTime, 1000);
+
+// Define um intervalo para atualizar a cor de fundo a cada minuto (60000 milissegundos)
+setInterval(updateBackgroundColor, 60000);
 
 // Chama a função getClima a cada 15 minutos
 setInterval(getClima, 900000); // 900000ms = 15 minutos
@@ -89,12 +120,13 @@ setInterval(fetchMensagem, 3600000); // 3600000ms = 1 hora
 // Atribuir a função onLoad ao carregar a janela
 window.onload = onLoad;
 
+
 var chamarTela3 = document.getElementById('idBuscarTelaAlarmes')
 chamarTela3.addEventListener("click", tratamentoChamarTela3)
 function tratamentoChamarTela3() {
     if (document.getElementById("idBuscarTelaAlarmes").textContent == "Set alarm.")
-    window.location.href = "/alarmPage"
+        window.location.href = "/alarmPage"
 }
-        
+
 
 
